9d8064ac8a2b92c7c595607f93a523bf06936f91
alltidtrist <alltidtrist@icloud.com>
2025-12-14 15:34:35 +0300

    Update Firebase configuration with actual credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,20 @@
-// Firebase конфигурация
-// ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА ВАШИ ИЗ FIREBASE CONSOLE
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Инициализация Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCN4jczMlU-HikQb3_4Ub3RokBYEy96mbU",
+    authDomain: "test-patologiya-counter.firebaseapp.com",
+    databaseURL: "https://test-patologiya-counter-default-rtdb.firebaseio.com",
+    projectId: "test-patologiya-counter",
+    storageBucket: "test-patologiya-counter.firebasestorage.app",
+    messagingSenderId: "337495881620",
+    appId: "1:337495881620:web:81f6b3d049913702878181"
+  };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
